486b6c7c67269275b41ca6f4784099fed3022671
stickoking <stickoking@hotmail.com>
2020-12-21 00:26:58 +0800

    EJS template & Layout Added

--- app.js
+++ app.js
@@ -4,23 +4,46 @@ const moment = require('moment');
 
 const app = express();
 let tasks = [];
+let workTask = [];
 app.use(bodyPraser.urlencoded({extended: true}));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
     const today = moment().format('dddd Do MMM YYYY');
-    //let day = '';
+    
 
-    res.render('list', {kindOfDay: today, newTask: tasks});
+    res.render('list', {listTitle: today, newTask: tasks});
 });
+
+app.get('/work', (req, res) => {
+    res.render('list', {listTitle: 'Work List', newTask: workTask})
+});
+
+// app.post('/work', (req, res) => {
+//     const task = req.body.task;
+//     workTask.push(task);
+//     res.redirect('/work');
+    
+    
+
+// });
 
 app.post('/', (req, res) => {
     const task = req.body.task;
-    tasks.push(task)
-    //task = req.body.task;
-    res.redirect('/')
-    console.log(task);
+   
+    if(req.body.list === 'Work List'){
+        workTask.push(task);
+        res.redirect('/work')
+    } else {
+        tasks.push(task)
+        res.redirect('/')
+
+    }
+   
 });
+
+
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000');
